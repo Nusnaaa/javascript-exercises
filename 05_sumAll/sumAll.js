f4e0c917f268c359f76474d28ae8322c938f1553
Nusnaaa <137788297+Nusnaaa@users.noreply.github.com>
2023-09-17 17:46:06 +0100

    Started exercise 05
    I have been successful in the first 2 tests. Now to do the others!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(integer1,integer2) {
 
+    let sum
+    
+        for (i = integer1; i <= integer2; i++) {
+            if (i ==integer1) {
+                sum = i;
+            }
+            else {
+                sum = sum + i;
+            }
+        }
+
+    return sum
 };
 
 // Do not edit below this line
