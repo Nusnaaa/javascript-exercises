16f3ee03882caa515f7e86e929be4ba4f681c3fa
Nusnaaa <137788297+Nusnaaa@users.noreply.github.com>
2023-09-19 15:01:47 +0100

    Completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,28 @@
 const sumAll = function(integer1,integer2) {
 
     let sum
+
+    if (integer1 > integer2) {
+        largerNumber = integer1;
+        smallerNumber = integer2;
+    }
+    else {
+        largerNumber = integer2;
+        smallerNumber = integer1
+    }
+
+    if (integer1 < 0 | integer2 < 0 | typeof(integer1) != 'number' | typeof(integer2) != 'number') {
+        return 'ERROR'
+    }
     
-        for (i = integer1; i <= integer2; i++) {
-            if (i ==integer1) {
-                sum = i;
-            }
-            else {
-                sum = sum + i;
-            }
+    for (currentNumber = smallerNumber; currentNumber <= largerNumber; currentNumber++) {
+        if (currentNumber == smallerNumber) {
+            sum = currentNumber;
         }
+        else {
+            sum = sum + currentNumber;
+        }
+    }
 
     return sum
 };
