33b9e6c2d28e0c462ee4452a64e495f13c2ae91f
Nusnaaa <137788297+Nusnaaa@users.noreply.github.com>
2023-09-19 15:38:07 +0100

    Completed exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 100 == 0 && year % 400 !== 0) {
+        yearStatus = 'Not a Leap Year'
+    }
+    else if (year % 4 == 0 || year % 400 == 0) {
+        yearStatus = 'Leap Year'
+    }
+    else {
+        yearStatus = 'Not a Leap Year'
+    }
 
+    if (yearStatus == 'Leap Year') {
+        return true
+    }
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line
